311ae37f449962ec0e02ef09b10d30ad92f93ad9
Sergey Osipov <sipopo@yandex.ru>
2021-12-28 13:37:49 +0300

    ninja level 2 - exercise 1

--- main.go
+++ main.go
@@ -1,39 +1,79 @@
 package main
 
 import (
-	// "encoding/json"
 	"fmt"
+	"io"
 	"log"
-	"golang.org/x/crypto/bcrypt"
+	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
 )
 
+var users = make(map[string]string)
 
 func main() {
-	password := "12345678"
 
-	hash, err := hashPassword(password)
-	if err != nil {
-		log.Fatalf("something wrong with hash %w", err)
+	http.HandleFunc("/", baseEndPoint)
+	http.HandleFunc("/register", registerEndPoint)
+	if http.ListenAndServe(":8080", nil) != nil {
+		log.Fatalln("Can't listen address")
 	}
-	fmt.Printf("You hash of password is %s \n", hash)
-
-	err = comparePassword("password", hash)
-	if err != nil {
-		log.Fatalf("Password is not correct, error: %s", err)
-	}
-	log.Println("Everething ok !")
 }
 
-func hashPassword(password string) ([]byte, error){
-	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, err
-	}
-	return bs, nil
+func baseEndPoint(w http.ResponseWriter, r *http.Request) {
+	log.Println("show base web")
+	io.WriteString(w, showHTML())
 }
 
-func comparePassword(password string, hashedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
-	return err
+func registerEndPoint(w http.ResponseWriter, r *http.Request) {
+	log.Println("register web")
+	if r.Method != http.MethodPost {
+		return
+	}
+	username := r.PostFormValue("username")
+	password := r.PostFormValue("password")
+
+	hash, err := getHash([]byte(password))
+	if err != nil {
+		log.Printf("Can't get hash from password %w \n", err)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+	users[username] = string(hash)
+	printAllUsers()
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func getHash(password []byte) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	if err != nil {
+		return []byte(""), fmt.Errorf("can't generate hash %w", err)
+	}
+	return hash, nil
+}
+
+func printAllUsers() {
+	for u, p := range users {
+		log.Printf("username: %v, password: %v", u, p)
+	}
+}
+
+func showHTML() string {
+	html := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>HMAC Example</title>
+	</head>
+	<body>
+		<form action="/register" method="post">
+			<input type="username" name="username" />
+			<input type="password" name="password" />
+			<input type="submit" />
+		</form>
+	</body>
+	</html>`
+
+	return html
 }
